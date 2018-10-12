8b13079baeed21fceb0dac4f2e85337313ec3e10
Ahmed Essam <eng.eramax@gmail.com>
2018-10-12 14:33:11 +0300

    up1

--- src/Layout.js
+++ src/Layout.js
@@ -1,42 +1,50 @@
-import React from 'react';
-import axios from 'axios';
-import Tag from './Tag2';
+import React from 'react'
+import axios from 'axios'
+import Tag from './Tag2'
 
 export default class Layout extends React.Component {
   constructor(props) {
-    super(props);
-    this.Callhandler = this.Callhandler.bind(this);
+    super(props)
+    this.Callhandler = this.Callhandler.bind(this)
+    this.toggle = this.toggle.bind(this)
   }
   state = {
-    Layout: {}
-  };
-  componentDidMount() {
-    if (this.state.Layout.lenth > 0) return;
-    axios.get('https://api.myjson.com/bins/ztmho').then(res => {
-      let Layout = res.data;
-      console.log(Layout);
-      //console.log(Layout.Components);
-      this.setState({ Layout });
-    });
+    Layout: {},
+    modal: false
   }
-  changeLayout = () => {};
+  componentDidMount() {
+    if (this.state.Layout.lenth > 0) return
+    axios.get('https://api.myjson.com/bins/ztmho').then(res => {
+      let Layout = res.data
+      console.log(Layout)
+      //console.log(Layout.Components);
+      this.setState({ Layout })
+    })
+  }
+  changeLayout = () => {}
   alertx = () => {
-    alert('hello');
-  };
+    alert('hello')
+  }
 
+  toggle = () => {
+    this.setState({
+      modal: !this.state.modal
+    })
+  }
   Callhandler = method => {
     switch (method) {
       case 'UpdateLayout':
-        this.changeLayout();
-        break;
+        this.changeLayout()
+        break
       case 'alert':
-        this.alertx();
-        break;
+        this.alertx()
+        break
+      case 'toggle':
+        this.toggle()
+        break
     }
-  };
+  }
   render() {
-    return (
-      <Tag tag={this.state.Layout.Components} handler={this.Callhandler} />
-    );
+    return <Tag tag={this.state.Layout.Components} handler={this.Callhandler} />
   }
 }
